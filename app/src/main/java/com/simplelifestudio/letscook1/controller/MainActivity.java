73d65d8f86cd268fbfc77a3b1f9a1fcec995b6e7
Ivan Escudero <ivanescud@gmail.com>
2020-03-18 19:56:33 -0500

    creacion de paquetes de model y controller

--- app/src/main/java/com/simplelifestudio/letscook1/controller/MainActivity.java
+++ app/src/main/java/com/simplelifestudio/letscook1/controller/MainActivity.java
@@ -1,4 +1,4 @@
-package com.simplelifestudio.letscook1;
+package com.simplelifestudio.letscook1.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.simplelifestudio.letscook1.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
