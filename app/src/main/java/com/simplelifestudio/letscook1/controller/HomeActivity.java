1b89d267012ac8aea21b38e9366b8b6b62d47b03
Ivan Escudero <ivanescud@gmail.com>
2020-04-15 16:34:16 -0500

    home class

--- app/src/main/java/com/simplelifestudio/letscook1/controller/HomeActivity.java
+++ app/src/main/java/com/simplelifestudio/letscook1/controller/HomeActivity.java
@@ -6,14 +6,25 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.simplelifestudio.letscook1.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class HomeActivity extends AppCompatActivity {
 
-    ImageView but;
+    private CircleImageView userImgCIV;
+    private TextView userNameTV;
+    private ImageView recetaImgcell;
+    private ImageView tragosImgcell;
+    private ImageView topImgcell;
+    private ImageView profileImgcell;
+    private RecyclerView recetasRV;
+    private RecyclerView bebidasRV;
 
 
     @Override
@@ -21,16 +32,25 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        init();
 
 
-        but = findViewById(R.id.homeBotonRecetaBt);
+        
 
-        but.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),receta_detailActivity.class));
-            }
-        });
+    }
+
+
+
+    private  void init() {
+        userImgCIV = findViewById(R.id.homeUserImgIV);
+        userNameTV = findViewById(R.id.homeUserTileTV);
+        recetaImgcell = findViewById(R.id.homeBotonRecetaBt);
+         tragosImgcell = findViewById(R.id.homeBotonbebidasIV);
+          topImgcell = findViewById(R.id.homeBotontopIV);
+         profileImgcell = findViewById(R.id.homeBotonperfilIV);
+         recetasRV = findViewById(R.id.homerecetasRV);
+         bebidasRV = findViewById(R.id.homebebidasRV);
+
 
     }
 
