03290f84021425c2e06382caa3a738efd2e3fd1a
Richard Quiros <richard.quiroz@uip.edu.pa>
2020-04-21 22:08:31 -0500

    animaciones splash resultado activity

--- app/src/main/java/com/simplelifestudio/letscook1/controller/SeleccioneIngrediente.java
+++ app/src/main/java/com/simplelifestudio/letscook1/controller/SeleccioneIngrediente.java
@@ -1,15 +1,13 @@
 package com.simplelifestudio.letscook1.controller;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
@@ -18,46 +16,48 @@ import com.simplelifestudio.letscook1.adapters.GridIngredientesAdapter;
 import com.simplelifestudio.letscook1.model.Ingredientes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class SeleccioneIngrediente extends AppCompatActivity {
+public class SeleccioneIngrediente{
+    private Context context;
   private TabLayout tabLayout;
   private GridView gridview;
   private TextView textViewlabeL;
   private   GridIngredientesAdapter gridIngredientesAdapter;
-  private ArrayList<Ingredientes> ingredientesCarnes;
+    private FloatingActionButton  floatingActionButton;
     private ArrayList<Ingredientes> ingredientesVegetales;
+    private ArrayList<Ingredientes> ingredientesCarnes;
     private ArrayList<Ingredientes> ingredientesFrutas;
     private ArrayList<Ingredientes> ingredientesGranos;
     private ArrayList<Ingredientes> ingredientesHierbas;
     private ArrayList<Ingredientes> ingredientesLacteos;
-    private ArrayList<Ingredientes> ingredientes;
-    private ArrayList<String> label;
+    private Map<String,Boolean> label;
     private int tipoIngrediente;
-    private FloatingActionButton  floatingActionButton;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.seleccion_ingredientes);
-        init();
-        obtenerDatos();
-        tablayoutListener();
-        gridviewItemListener();
-        flotingbuttomActionListener();
+    private int datos =0;
+
+    public SeleccioneIngrediente(Context context, TabLayout tabLayout, GridView gridview, TextView textViewlabeL, GridIngredientesAdapter gridIngredientesAdapter, FloatingActionButton floatingActionButton) {
+        this.context = context;
+        this.tabLayout = tabLayout;
+        this.gridview = gridview;
+        this.textViewlabeL = textViewlabeL;
+        this.gridIngredientesAdapter = gridIngredientesAdapter;
+        this.floatingActionButton = floatingActionButton;
     }
 
+
     public void init(){
-        tabLayout = findViewById(R.id.seleccionIngredientesTabLayout);
-        gridview = findViewById(R.id.seleccion_ingredientesGridView);
-        textViewlabeL = findViewById(R.id.seleccionIngredientesLabelTV);
         ingredientesCarnes = new ArrayList<Ingredientes>();
         ingredientesVegetales = new ArrayList<Ingredientes>();
         ingredientesFrutas = new ArrayList<>();
         ingredientesGranos = new ArrayList<>();
         ingredientesHierbas = new ArrayList<>();
         ingredientesLacteos = new ArrayList<>();
-        ingredientes = new ArrayList<>();
-        label = new ArrayList<>();
-        floatingActionButton = findViewById(R.id.selecciomIngredientesFlotingButtom);
+        label = new HashMap<>();
+        obtenerDatos();
+        tablayoutListener();
+        gridviewItemListener();
+        flotingbuttomActionListener();
     }
 
     public void obtenerDatos(){
@@ -65,23 +65,36 @@ public class SeleccioneIngrediente extends AppCompatActivity {
         ingredientesCarnes.add(new Ingredientes("Pollo",R.drawable.carnepollo));
         ingredientesCarnes.add(new Ingredientes("Carne vegana",R.drawable.carnevegeta));
         ingredientesCarnes.add(new Ingredientes("Cerdo",R.drawable.carnecerdo));
-        ingredientesCarnes.add(new Ingredientes("Pescado",R.drawable.carnepescado));;
+        ingredientesCarnes.add(new Ingredientes("Pescado",R.drawable.carnepescado));
 
 
-        ingredientesVegetales.add(new Ingredientes("Maiz",R.drawable.carnepescado));
+        ingredientesVegetales.add(new Ingredientes("Apio",R.drawable.ingre_apio));
+        ingredientesVegetales.add(new Ingredientes("Brocoli",R.drawable.ingre_brocoli));
+        ingredientesVegetales.add(new Ingredientes("Coliflor",R.drawable.ingre_colifor));
+        ingredientesVegetales.add(new Ingredientes("Zanahoria",R.drawable.ingre_zanahoria));
 
 
-        ingredientesFrutas.add(new Ingredientes("Manzana",R.drawable.carnepescado));
+        ingredientesFrutas.add(new Ingredientes("Banana",R.drawable.ingre_banana));
+        ingredientesFrutas.add(new Ingredientes("Uvas",R.drawable.ingre_uva));
+        ingredientesFrutas.add(new Ingredientes("Cerezas",R.drawable.ingre_cerezas));
+        ingredientesFrutas.add(new Ingredientes("Kiwi",R.drawable.ingre_kiwi));
 
-        ingredientesFrutas.add(new Ingredientes("Manzana",R.drawable.carnepescado));
+        ingredientesGranos.add(new Ingredientes("Arroz",R.drawable.ingre_arroz));
+        ingredientesGranos.add(new Ingredientes("Mijo",R.drawable.ingre_mijo));
+        ingredientesGranos.add(new Ingredientes("Arroz integral",R.drawable.ingre_arrozintegral));
+        ingredientesGranos.add(new Ingredientes("Cereal",R.drawable.ingre_cereal));
 
-        ingredientesGranos.add(new Ingredientes("Arroz",R.drawable.carnepescado));
+        ingredientesHierbas.add(new Ingredientes("Perejil",R.drawable.ingre_perejil));
+        ingredientesHierbas.add(new Ingredientes("Romero",R.drawable.ingre_romero));
+        ingredientesHierbas.add(new Ingredientes("Clavo de olor",R.drawable.ingre_clavoolor));
+        ingredientesHierbas.add(new Ingredientes("Vainilla",R.drawable.ingre_vainilla));
 
-        ingredientesHierbas.add(new Ingredientes("Perejil",R.drawable.carnepescado));
+        ingredientesLacteos.add(new Ingredientes("Leche",R.drawable.ingre_leche));
+        ingredientesLacteos.add(new Ingredientes("Yogurt",R.drawable.ingre_yogurth));
+        ingredientesLacteos.add(new Ingredientes("Mantequilla",R.drawable.ingre_matequilla));
+        ingredientesLacteos.add(new Ingredientes("Queso",R.drawable.ingre_queso));
 
-        ingredientesLacteos.add(new Ingredientes("Leche",R.drawable.carnepescado));
-
-        gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesCarnes,R.layout.grid_ingredientes);
+        gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesCarnes,R.layout.grid_ingredientes);
         gridview.setAdapter(gridIngredientesAdapter);
     }
 
@@ -92,31 +105,31 @@ public class SeleccioneIngrediente extends AppCompatActivity {
                 tipoIngrediente = tab.getPosition();
               switch (tab.getPosition()){
                   case 0:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesCarnes,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesCarnes,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
                   case 1:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesVegetales,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesVegetales,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
                   case 2:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesFrutas,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesFrutas,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
                   case 3:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesGranos,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesGranos,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
                   case 4:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesHierbas,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesHierbas,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
                   case 5:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesLacteos,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesLacteos,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
                   default:
-                      gridIngredientesAdapter = new GridIngredientesAdapter(getApplicationContext(),ingredientesCarnes,R.layout.grid_ingredientes);
+                      gridIngredientesAdapter = new GridIngredientesAdapter(context,ingredientesCarnes,R.layout.grid_ingredientes);
                       gridview.setAdapter(gridIngredientesAdapter);
                       break;
               }
@@ -140,126 +153,99 @@ public class SeleccioneIngrediente extends AppCompatActivity {
                boolean add=true;
                switch (tipoIngrediente){
                    case 0:
-                       for(String j:label){
-                           if(j==ingredientesCarnes.get(i).getNombreIN()){
-                               label.remove(j);
-                               add = false;
-                           }
-                       }
-                       if(add) {
-                           label.add(ingredientesCarnes.get(i).getNombreIN());
-                       }
-
-                       textolabelSet();
+                         if(label.get((ingredientesCarnes.get(i).getNombreIN()))!=null){
+                              label.remove(ingredientesCarnes.get(i).getNombreIN());
+                          }
+                          else {
+                              label.put(ingredientesCarnes.get(i).getNombreIN(),true);
+                          }
                        break;
                    case 1:
-                       for(String j:label){
-                           if(j==ingredientesVegetales.get(i).getNombreIN()){
-                               label.remove(j);
-                               add = false;
-                           }
+                       if(label.get(ingredientesVegetales.get(i).getNombreIN())!=null){
+                           label.remove(ingredientesVegetales.get(i).getNombreIN());
                        }
-                       if(add) {
-                           label.add(ingredientesVegetales.get(i).getNombreIN());
+                       else {
+                           label.put(ingredientesVegetales.get(i).getNombreIN(),true);
                        }
-
-                       textolabelSet();
                        break;
                    case 2:
-                       for(String j:label){
-                           if(j==ingredientesFrutas.get(i).getNombreIN()){
-                               label.remove(j);
-                               add = false;
-                           }
+                       if(label.get(ingredientesFrutas.get(i).getNombreIN())!=null){
+                           label.remove(ingredientesFrutas.get(i).getNombreIN());
                        }
-                       if(add) {
-                           label.add(ingredientesFrutas.get(i).getNombreIN());
+                       else {
+                           label.put(ingredientesFrutas.get(i).getNombreIN(),true);
                        }
-
-                       textolabelSet();
                        break;
                    case 3:
-                       for(String j:label){
-                           if(j==ingredientesGranos.get(i).getNombreIN()){
-                               label.remove(j);
-                               add = false;
-                           }
+                       if(label.get(ingredientesGranos.get(i).getNombreIN())!=null){
+                           label.remove(ingredientesGranos.get(i).getNombreIN());
                        }
-                       if(add) {
-                           label.add(ingredientesGranos.get(i).getNombreIN());
+                       else {
+                           label.put(ingredientesGranos.get(i).getNombreIN(),true);
                        }
-
-                       textolabelSet();
                        break;
-                   case 4:for(String j:label){
-                       if(j==ingredientesHierbas.get(i).getNombreIN()){
-                           label.remove(j);
-                           add = false;
+                   case 4:
+                       if(label.get(ingredientesGranos.get(i).getNombreIN())!=null){
+                           label.remove(ingredientesGranos.get(i).getNombreIN());
                        }
-                   }
-                       if(add) {
-                           label.add(ingredientesHierbas.get(i).getNombreIN());
+                       else {
+                           label.put(ingredientesGranos.get(i).getNombreIN(),true);
                        }
-
-                       textolabelSet();
                        break;
                    case 5:
-                       for(String j:label){
-                           if(j==ingredientesLacteos.get(i).getNombreIN()){
-                               label.remove(j);
-                               add = false;
-                           }
+                       if(label.get(ingredientesGranos.get(i).getNombreIN())!=null){
+                           label.remove(ingredientesGranos.get(i).getNombreIN());
                        }
-                       if(add) {
-                           label.add(ingredientesLacteos.get(i).getNombreIN());
+                       else {
+                           label.put(ingredientesGranos.get(i).getNombreIN(),true);
                        }
-
-                       textolabelSet();
                        break;
                    default:
-                       for(String j:label){
-                           if(j==ingredientesCarnes.get(i).getNombreIN()){
-                               label.remove(j);
-                               add = false;
-                           }
+                       if(label.get(ingredientesCarnes.get(i).getNombreIN())!=null){
+                           label.remove(ingredientesCarnes.get(i).getNombreIN());
                        }
-                       if(add) {
-                           label.add(ingredientesCarnes.get(i).getNombreIN());
+                       else {
+                           label.put(ingredientesCarnes.get(i).getNombreIN(),true);
                        }
-
-                       textolabelSet();
                        break;
 
                }
-
+            textolabelSet();
             }
         });
     }
 
     public void textolabelSet(){
         textViewlabeL.setText("");
-        for(String i : label){
-            if(textViewlabeL.getText().toString().isEmpty()){
-                textViewlabeL.setText(i);
-            }
-            else {
-                textViewlabeL.setText(textViewlabeL.getText() + "," + i);
-            }
+        datos = 0;
+           for(String key : label.keySet()) {
+               datos++;
+               if (textViewlabeL.getText().toString().isEmpty()) {
+                   textViewlabeL.setText(key);
+               } else {
+                   textViewlabeL.setText(textViewlabeL.getText() + "," + key);
+               }
+           }
         }
-    }
 
     public void flotingbuttomActionListener(){
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String[] listalabel = new String[datos];
+                int i = 0;
                 if(label.isEmpty()) {
-                    Toast.makeText(getApplicationContext(),"No tienes ingredientes sufucientes",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(context,"No tienes ingredientes sufucientes",Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    for(String key : label.keySet()) {
+                        listalabel[i] = key;
+                        i++;
+                    }
                     Bundle bundle = new Bundle();
-                    bundle.putSerializable("label", label);
-                    Intent intent = new Intent(getApplicationContext(), ResultadoBusqueda.class);
-                    startActivity(intent);
+                    bundle.putStringArray("listalabel",listalabel);
+                    Intent intent = new Intent(context, ResultadoBusqueda.class).putExtras(bundle);
+                    context.startActivity(intent);
                 }
             }
         });
